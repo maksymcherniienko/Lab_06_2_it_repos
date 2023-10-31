345009933e22b15385064cef23f14cd35028d2fb
Maksym Cherniienko <h0nza4ok@gmail.com>
2023-10-31 20:59:24 +0200

    Add project files.

---
+++ Lab_06_2_it/Lab_06_2it.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+#include<iomanip>
+using namespace std;
+
+void Create(int* a, const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		a[i] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int* a, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << a[i];
+}
+
+int Find(int* a, const int size, int i, int smallestOdd, int smallestOddIndex)
+{
+	
+	for (int i = 0; i < size; i++) 
+	{
+		if (a[i] % 2 != 0) {
+			if (smallestOdd == -1 || a[i] < smallestOdd) {
+				smallestOdd = a[i];
+				smallestOddIndex = i;
+			}
+		}
+	}
+	return smallestOddIndex;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 10;
+	int a[n];
+	int Low = 1;
+	int High = 31;
+	Create(a, n, Low, High);
+	cout << "mas = ["; Print(a, n); cout << "  ]" << endl;
+	cout << "Index of the smallest odd: [" << Find(a, n, 0, -1, -1) << "]" << endl;
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_06_2_it/Lab_06_2it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 10;
+			int a[n] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+			int S = 0;
+
+			int result = Find(a, n, 0, -1, -1);
+
+			Assert::AreEqual(Find(a, n, 0, -1, -1), S, 1e-1);
+		}
+	};
+}
